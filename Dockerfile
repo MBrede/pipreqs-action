fe05b6b75ebd46a954a8b40b20d31f8d0b46a44d
Max Brede <50902832+MBrede@users.noreply.github.com>
2023-05-30 16:45:13 +0200

    added gh to workflow

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,7 @@ FROM alpine:3.17
 
 COPY LICENSE README.md collect_reqs.py entrypoint.sh /
 
-RUN apk add --no-cache git
+RUN apk add --no-cache git github-cli
 RUN apk add --no-cache python3 py3-pip
 RUN pip3 install pipreqs
 
--- entrypoint.sh
+++ entrypoint.sh
@@ -5,6 +5,12 @@ python /collect_reqs.py $INPUT_REQUIREMENT_PATH $INPUT_PROJECT_PATH $INPUT_RECUR
 set -e
 sh -c "ls"
 
+if[$INPUT_TOKEN]
+then
+  sh -c "echo '${INPUT_TOKEN}' > token.txt"
+  sh -c "gh auth login --with-token < token.txt"
+fi
+
 sh -c "git config --global --add safe.directory /github/workspace && git config --global user.name '${GITHUB_ACTOR}' \
       && git config --global user.email '${GITHUB_ACTOR}@users.noreply.github.com' \
       && git add requirements.txt && git commit -m 'Updated requirements file.' --allow-empty \
