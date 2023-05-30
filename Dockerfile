3df29286702739ca92d2ff52b1ca04e0d232f0b2
Max Brede <50902832+MBrede@users.noreply.github.com>
2023-05-30 14:01:41 +0200

    fixed missing script in Dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,12 @@
 FROM alpine:3.17
 
-COPY LICENSE README.md entrypoint.sh /
+COPY LICENSE README.md collect_reqs.py entrypoint.sh /
 
 RUN apk add --no-cache git
 RUN apk add --no-cache python3 py3-pip
 RUN pip3 install pipreqs os sys
 
 RUN chmod +x /entrypoint.sh
+RUN chmod +x /collect_reqs.py
 
 ENTRYPOINT ["/entrypoint.sh"]
--- collect_reqs.py
+++ collect_reqs.py
@@ -10,12 +10,12 @@ def main(args):
     reqs = []
     if args[2]:
         result += [os.path.join(dp, f) for dp, dn, filenames in os.walk(args[1])
-                   for f in filenames if os.path.splitext(f)[1] == '.py']
+                   for f in filenames if os.path.splitext(f)[1] == '.py' and f[0]]
     for path in result:
         os.system(f'pipreqs --force --savepath {args[0]} {path}')
         with open(args[0], 'r') as f:
             reqs += f.readlines()
-    reqs = list(set(reqs))
+    reqs = [req for req in list(set(reqs)) if req]
     with open(args[0], 'w') as f:
         f.writelines(reqs)
 
